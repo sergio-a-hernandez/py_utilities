9d7b60113b3a7d31fb3c9aa0eeb5ea32648f34d7
sergio-a-hernandez <153755531+sergio-a-hernandez@users.noreply.github.com>
2023-12-14 15:07:24 -0700

    Create string.py
    # String Class
    
    The `String` class is a Python implementation designed to enhance string manipulation operations. It offers a variety of methods for common string operations, along with additional functionality for concatenation, subtraction, and conversion to numeric values.
    
    ## Features
    
    ### String Operations
    - **clone:** Create a copy of the string.
    - **compare_to:** Compare two strings and return an integer value (0 for true, 1 for false).
    - **contains:** Check if a specified substring exists within the string.
    - **ends_with:** Check if the string ends with a specified value.
    - **equals:** Compare two strings for equality.
    - **get_hash_code:** Return the hash code of the string.
    - **get_type:** Return the type of the string.
    - **index_of:** Return the index position of the first occurrence of a specified character.
    - **to_lower:** Convert the string to lowercase.
    - **to_upper:** Convert the string to uppercase.
    - **insert:** Insert a substring into the string at a specified position.
    - **last_index_of:** Return the index position of the last occurrence of a specified character.
    - **length:** Return the length of the string.
    - **remove:** Delete characters from the beginning up to a specified index.
    - **replace:** Replace a character in the string.
    - **split:** Split the string based on a specified delimiter.
    - **starts_with:** Check if the string starts with a specified character.
    - **substring:** Return a substring.
    - **to_char_array:** Convert the string into a character array.
    - **trim:** Remove extra whitespaces from the beginning and end of the string.
    
    ### Concatenation and Subtraction
    - **Concatenation (+):** Concatenate two instances of `String`.
    - **Subtraction (-):** Remove a specified substring from the original string.
    
    ### Numeric Conversion
    - **ToValue:** Attempt to convert the string to a numeric value, supporting decimal, octal, and hexadecimal formats.
    
    ## Usage
    
    ```python
    # Example Usage
    sf = StringFunctions("Hello, World!")
    result = sf.to_value()
    print("Numeric Value:", result)
    ```
    Feel free to use and extend the StringFunctions class for enhanced string manipulation in your Python projects.
    
    This example covers the class features, methods, and usage examples. Adjust it based on your specific implementation and any additional details you want to highlight.

---
+++ string.py
@@ -0,0 +1,162 @@
+class String:
+    def __init__(self, value):
+        self.value = value
+
+    def __add__(self, other):
+        if isinstance(other, String):
+            return String(self.value + other.value)
+        elif isinstance(other, str):
+            return String(self.value + other)
+        else:
+            raise TypeError("Unsupported operand type for +: {}".format(type(other)))
+
+    def __sub__(self, other):
+        if isinstance(other, String):
+            return String(self.value.replace(other.value, ""))
+        if isinstance(other, str):
+            return String(self.value.replace(other, ""))
+        else:
+            raise TypeError("Unsupported operand type for -: {}".format(type(other)))        
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return repr(self.value)
+
+    def clone(self):
+        return self.value[:]
+
+    def compare_to(self, other):
+        return 0 if self.value == other else 1
+
+    def contains(self, sub_str):
+        return sub_str in self.value
+
+    def ends_with(self, end_str):
+        return self.value.endswith(end_str)
+
+    def equals(self, other):
+        return self.value == other
+
+    def get_hash_code(self):
+        return hash(self.value)
+
+    def get_type(self):
+        return type(self.value)
+
+    def index_of_char(self, char):
+        return self.value.index(char)
+
+    def to_lower(self):
+        return self.value.lower()
+
+    def to_upper(self):
+        return self.value.upper()
+
+    def insert(self, index, insert_str):
+        return self.value[:index] + insert_str + self.value[index:]
+
+    def last_index_of_char(self, char):
+        return self.value.rindex(char)
+
+    def length_of(self):
+        return len(self.value)
+
+    def remove(self, index):
+        return self.value[:index]
+
+    def replace(self, old_char, new_char):
+        return self.value.replace(old_char, new_char)
+
+    def split(self, delimiter):
+        return self.value.split(delimiter)
+
+    def starts_with_char(self, char):
+        return self.value.startswith(char)
+
+    def substring(self, start, end):
+        return self.value[start:end]
+
+    def to_char_array(self):
+        return list(self.value)
+
+    def trim(self):
+        return self.value.strip()
+
+    def to_value(self):
+        # Oct
+        if self.value.startswith("0") and len(self.value) > 1:
+            try:
+                return int(self.value, 8)
+            except ValueError:
+                pass
+            
+        # Int
+        try:
+            return int(self.value)
+        except ValueError:
+            pass
+
+        # Hex
+        if self.value.startswith("0x"):
+            try:
+                return int(self.value, 16)
+            except ValueError:
+                pass
+
+        # Si no es posible convertir, devolver None
+        return None
+
+
+if __name__ == "__main__":
+    firstname = String("Steven Clark")
+    lastname = String("Clark")
+
+    fn = "Steven Clark"
+    ln = "Clark"
+
+    print (firstname + lastname)
+    print (firstname + ln)
+    print (firstname - lastname)
+    print (firstname - ln)
+
+    print(firstname.clone())
+    print(firstname.compare_to(lastname))
+    print(firstname.contains("ven"))
+    print(firstname.ends_with("n"))
+    print(firstname.equals(lastname))
+    print(firstname.get_hash_code())
+    print(firstname.get_type())
+    print(firstname.index_of_char("e"))
+    print(firstname.to_lower())
+    print(firstname.to_upper())
+    print(firstname.insert(0, "Hello"))
+    print(firstname.last_index_of_char("e"))
+    print(firstname.length_of())
+    print(firstname.remove(5))
+    print(firstname.replace('e', 'i'))
+    
+    split = firstname.split('e')
+    for part in split:
+        print(part)
+
+    print(firstname.starts_with_char("S"))
+    print(firstname.substring(2, 7))
+    print(firstname.to_char_array())
+    print(firstname.trim())
+
+    numero_decimal = String("1234")
+    numero_octal = String("01234")
+    numero_hexadecimal = String("0x1234")
+    texto_no_numerico1 = String("abc123")
+    texto_no_numerico2 = String("0abc123")
+    texto_no_numerico3 = String("0xgbc123")
+
+    print("Convertir a valor:")
+    print("Decimal:", numero_decimal.to_value())
+    print("Octal:", numero_octal.to_value())
+    print("Hexadecimal:", numero_hexadecimal.to_value())
+    print("Texto no numérico:", texto_no_numerico1.to_value())
+    print("Texto no numérico:", texto_no_numerico2.to_value())
+    print("Texto no numérico:", texto_no_numerico3.to_value())
